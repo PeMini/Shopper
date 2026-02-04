1e50d533d2b45ad5d40e51defc79b2fb31c34318
Pemini Imasha <pemini139@gmail.com>
2026-02-04 12:29:27 +0530

    add admin panel and modified index.js

--- backend/index.js
+++ backend/index.js
@@ -114,6 +114,15 @@ app.post('/removeproduct',async (req,res)=>{
     })
 })
 
+//creating API for getting all products
+app.get('/allproducts',async(req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
+
+
 app.listen(port,(error)=>{
     if (!error) {
         console.log("Server Running on Port "+port)
