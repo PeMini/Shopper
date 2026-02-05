53cfe10cd85c1cfdf62e3d606002abc13f2133d3
Pemini Imasha <pemini139@gmail.com>
2026-02-06 00:22:48 +0530

    created endpoint for register user

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const jwt = require ("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
 
 app.use(express.json());
 app.use(cors());
@@ -137,7 +138,57 @@ app.get('/allproducts',async(req,res)=>{
     res.send(products);
 })
 
+//Shema creating for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
 
+//Creating endpoint for registring the user
+app.post('/signup',async(req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if (check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0; 
+    }
+    const user = new Users ({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token =jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+//
 
 app.listen(port,(error)=>{
     if (!error) {
